023c3584fa2eadb12c3f806797f9ca6b5825c5c0
BoyanDashev <b.dashevv@gmail.com>
2024-05-16 05:41:43 -0400

    New Data

---
+++ Scope/scope_lab.js
@@ -0,0 +1,16 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+// Block scope
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";
+const blockConst = "I'm a block-scoped const";
+}
+// Global scope
+console.log(globalVar); // Output: "I'm a global variable"
+console.log(globalLet); // Output: "I'm also global, but scoped with let"
+console.log(globalConst); // Output: "I'm a global constant"
